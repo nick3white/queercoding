3666a5c977265b8e5e4e520c90fae8b0a4a8ee3a
Nick White <nick33white@gmail.com>
2023-11-01 21:52:18 -0500

    add "content" to index

--- src/components/Header/header.tsx
+++ src/components/Header/header.tsx
@@ -23,6 +23,10 @@ export const Header = component$(() => {
           background: "var(--background)",
           border: "2px solid var(--foreground)",
           display: "flex",
+          flexDirection: "column",
+          lg: {
+            flexDirection: "row",
+          },
           justifyContent: "space-evenly",
           alignItems: "center",
           gap: "11px",
@@ -31,7 +35,29 @@ export const Header = component$(() => {
       >
         <h2>Welcome to</h2>
         <h1>Queer Coding</h1>
-        <ul class="nav-list">
+        <ul
+          class={css({
+            listStyleType: "none",
+            display: "flex",
+            flexDirection: "row",
+            lg: {
+              flexDirection: "column",
+            },
+            "& a": {
+              height: "50px",
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              paddingInline: "7px",
+              border: "2px solid var(--color3)",
+              transition: "200ms",
+              "&:hover": {
+                background: "var(--color6)",
+                color: "var(--background)",
+              },
+            },
+          })}
+        >
           {navlinks.map((link) => (
             <li key={link}>
               <a href={link}>{link}</a>
